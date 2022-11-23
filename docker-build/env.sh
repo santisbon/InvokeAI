e37d09ac503a7e28a2c274829ba27b4b9c33100f
Armando C. Santisbon <3804335+santisbon@users.noreply.github.com>
2022-11-23 01:13:14 -0600

    Use .env file

--- docker-build/env.sh
+++ docker-build/env.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+#####################################################
+# Example .env file
+#####################################################
+# HUGGINGFACE_TOKEN=xxxxxxxxx
+# ARCH=aarch64
+# INVOKEAI_CONDA_ENV_FILE=environment-lin-aarch64.yml
+# INVOKEAI_GIT=invoke-ai/InvokeAI
+# INVOKEAI_BRANCH=main
+#####################################################
+
+FILE=./docker-build/.env
+
+if [[ -f "$FILE" ]]; then
+  echo "$FILE exists and contains: "
+  # Show env vars
+  grep -v '^#' "$FILE"
+  echo
+  # Export env vars
+  set -o allexport
+  source "$FILE"
+  set +o allexport
+fi
+
 project_name=${PROJECT_NAME:-invokeai}
 volumename=${VOLUMENAME:-${project_name}_data}
 arch=${ARCH:-x86_64}
